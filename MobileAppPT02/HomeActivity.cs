9fef596e9fe6230178f6d404dc0ead71f31d462a
johnpaunlagui <johnpaunlagui@outlook.com>
2019-02-24 19:14:44 +0800

    Add project files.

---
+++ MobileAppPT02/HomeActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MobileAppPT02
+{
+    [Activity(Label = "HomeActivity", Theme = "@style/AppTheme")]
+    public class HomeActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.activity_home);
+        }
+
+        public override void OnBackPressed()
+        {
+            AlertDialog.Builder alertdiag = new AlertDialog.Builder(this);
+            alertdiag.SetTitle("Confirm Exit");
+            alertdiag.SetMessage("Do you want to Exit?");
+            alertdiag.SetPositiveButton("Yes", (senderAlert, args) => {
+                OnDestroy();
+                Finish();
+            });
+            alertdiag.SetNegativeButton("No", (senderAlert, args) => {
+                alertdiag.Dispose();
+                Toast.MakeText(this, "Thank you for Staying, \n Unlike your EX hahaha", ToastLength.Short).Show();
+            });
+            Dialog diag = alertdiag.Create();
+            diag.Show();
+        }
+    }
+}
---
+++ MobileAppPT02/SplashScreenActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Util;
+using System.Threading.Tasks;
+using Android.Content;
+
+namespace MobileAppPT02
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X: " + typeof(SplashScreenActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistableBundle)
+        {
+            base.OnCreate(savedInstanceState, persistableBundle);
+            Log.Debug(TAG, "SplashScreenActivity.OnCreate");
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task Startup = new Task(() => { SimulateStartup(); });
+            Startup.Start();
+        }
+        public override void OnBackPressed() { }
+
+        async void SimulateStartup()
+        {
+            Log.Debug(TAG, "Performing Start Up");
+            await Task.Delay(4000);
+            Log.Debug(TAG, "Starting HomeActivity");
+            StartActivity(new Intent(Application.Context, typeof(HomeActivity)));
+        }
+    }
+}
